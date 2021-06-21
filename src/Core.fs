24ec6fad53c4d5e4df7e064147cbb1ee21e5b247
Franco Ponticelli <franco.ponticelli@gmail.com>
2021-06-21 07:31:27 +0000

    Move 'I to Template type parameters.

--- src/Core.fs
+++ src/Core.fs
@@ -15,10 +15,10 @@ module Value =
     let derived f = Derived f
     let ofState = Derived id
 
-type Template<'N, 'S, 'A, 'Q> =
+type Template<'N, 'I, 'S, 'A, 'Q> =
     | Node of 'N
-    | Fragment of Template<'N, 'S, 'A, 'Q> list
-    | MapState of IMapState<'N, 'S, 'A, 'Q>
+    | Fragment of Template<'N, 'I, 'S, 'A, 'Q> list
+    | MapState of IMapState<'N, 'I, 'S, 'A, 'Q>
 
 and View<'S, 'Q, 'I> =
     { Impl: 'I option
@@ -26,40 +26,41 @@ and View<'S, 'Q, 'I> =
       Destroy: unit -> unit
       Query: 'Q -> unit }
 
-and IMapState<'N, 'S, 'A, 'Q> =
-    abstract Accept : IMapStateInvoker<'N, 'S, 'A, 'Q, 'R> -> 'R
+and IMapState<'N, 'I, 'S, 'A, 'Q> =
+    abstract Accept : IMapStateInvoker<'N, 'I, 'S, 'A, 'Q, 'R> -> 'R
 
-and MapState<'N, 'S, 'S2, 'A, 'Q>(m, t, mrn) =
+and MapState<'N, 'I, 'S, 'S2, 'A, 'Q>(m, t, mrn) =
     member this.MapF : 'S -> 'S2 = m
-    member this.Template : Template<'N, 'S2, 'A, 'Q> = t
-    member this.MakeRenderNode : MakeNodeRender<'N, 'S2, 'Q, 'I> = mrn
+    member this.Template : Template<'N, 'I, 'S2, 'A, 'Q> = t
+    member this.MakeRenderNode : MakeNodeRender<'N, 'I, 'S2, 'Q> = mrn
     with
-        interface IMapState<'N, 'S, 'A, 'Q> with
+        interface IMapState<'N, 'I, 'S, 'A, 'Q> with
             member this.Accept f = f.Invoke<'S2> this
 
-and IMapStateInvoker<'N, 'S, 'A, 'Q, 'R> =
-    abstract Invoke<'S2> : MapState<'N, 'S, 'S2, 'A, 'Q> -> 'R
+and IMapStateInvoker<'N, 'I, 'S, 'A, 'Q, 'R> =
+    abstract Invoke<'S2> : MapState<'N, 'I, 'S, 'S2, 'A, 'Q> -> 'R
 
 and Render<'S, 'Q, 'I> = 'I -> 'S -> View<'S, 'Q, 'I>
 
-and MakeNodeRender<'N, 'S, 'Q, 'I> = 'N -> Render<'S, 'Q, 'I>
+and MakeNodeRender<'N, 'I, 'S, 'Q> = 'N -> Render<'S, 'Q, 'I>
 
-let packMapState (mapState: MapState<'N, 'S, 'S2, 'A, 'Q>) = mapState :> IMapState<'N, 'S, 'A, 'Q>
+let packMapState (mapState: MapState<'N, 'I, 'S, 'S2, 'A, 'Q>) =
+    mapState :> IMapState<'N, 'I, 'S, 'A, 'Q>
 
-let unpackMapState (mapState: IMapState<'N, 'S, 'A, 'Q>) (f: IMapStateInvoker<'N, 'S, 'A, 'Q, 'R>) : 'R =
+let unpackMapState (mapState: IMapState<'N, 'I, 'S, 'A, 'Q>) (f: IMapStateInvoker<'N, 'I, 'S, 'A, 'Q, 'R>) : 'R =
     mapState.Accept f
 
-let makeMapState<'N, 'S, 'S2, 'A, 'Q>
-    (makeNodeRender: MakeNodeRender<'N, 'S2, 'Q, 'I>)
+let makeMapState<'N, 'I, 'S, 'S2, 'A, 'Q>
+    (makeNodeRender: MakeNodeRender<'N, 'I, 'S2, 'Q>)
     (f: 'S -> 'S2)
-    (t: Template<'N, 'S2, 'A, 'Q>)
+    (t: Template<'N, 'I, 'S2, 'A, 'Q>)
     =
     packMapState
-    <| MapState<'N, 'S, 'S2, 'A, 'Q>(f, t, makeNodeRender)
+    <| MapState<'N, 'I, 'S, 'S2, 'A, 'Q>(f, t, makeNodeRender)
 
-let rec makeRender<'N, 'S, 'A, 'Q, 'I>
-    (makeNodeRender: MakeNodeRender<'N, 'S, 'Q, 'I>)
-    (template: Template<'N, 'S, 'A, 'Q>)
+let rec makeRender<'N, 'I, 'S, 'A, 'Q>
+    (makeNodeRender: MakeNodeRender<'N, 'I, 'S, 'Q>)
+    (template: Template<'N, 'I, 'S, 'A, 'Q>)
     : Render<'S, 'Q, 'I> =
     match template with
     | Node n -> makeNodeRender n
@@ -75,16 +76,23 @@ let rec makeRender<'N, 'S, 'A, 'Q, 'I>
               Query = fun q -> List.iter (fun i -> i.Query q) views }
     | MapState mapState -> makeRenderMapState mapState
 
-and makeRenderMapState<'N, 'S1, 'S2, 'A, 'Q, 'I> (mapState: IMapState<'N, 'S1, 'A, 'Q>) : Render<'S1, 'Q, 'I> =
+and makeRenderMapState<'N, 'I, 'S1, 'S2, 'A, 'Q> (mapState: IMapState<'N, 'I, 'S1, 'A, 'Q>) : Render<'S1, 'Q, 'I> =
     unpackMapState
         mapState
-        { new IMapStateInvoker<'N, 'S1, 'A, 'Q, Render<'S1, 'Q, 'I>> with
-            member __.Invoke<'S2>(mapState: MapState<'N, 'S1, 'S2, 'A, 'Q>) : Render<'S1, 'Q, 'I> =
+        { new IMapStateInvoker<'N, 'I, 'S1, 'A, 'Q, Render<'S1, 'Q, 'I>> with
+            member __.Invoke<'S2>(mapState: MapState<'N, 'I, 'S1, 'S2, 'A, 'Q>) : Render<'S1, 'Q, 'I> =
                 let t = mapState.Template
 
-                let render : Render<'S1, 'Q, 'I> = makeRender mapState.MakeRenderNode t
+                let render =
+                    makeRender<'N, 'I, 'S2, 'A, 'Q> mapState.MakeRenderNode t
 
-                fun (i: 'I) (s: 'S1) -> render i (mapState.MapF s) }
+                fun (i: 'I) (s: 'S1) ->
+                    let view = render i (mapState.MapF s)
+
+                    { Impl = view.Impl
+                      Query = view.Query
+                      Destroy = view.Destroy
+                      Change = fun s1 -> view.Change <| mapState.MapF s1 } }
 
 // type MapStateValue<'S1, 'S2, 'N when 'S1: equality and 'S2: equality> = ('S1 -> 'S2) * Template<'N, 'S2>
 
